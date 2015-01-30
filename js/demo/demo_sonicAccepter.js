8efbf1c5e68c36dd2aa7a7ba76aaf3550a42a4af
Arthus <yulhuang@cisco.com>
2015-01-30 15:23:54 +0800

    listen

--- js/demo/demo_sonicAccepter.js
+++ js/demo/demo_sonicAccepter.js
@@ -1,3 +1,20 @@
-(function(){
+window.onload=function(){
+    var sonic_panel = document.getElementById('sonic_panel');
+    var band = new Jsonic.Band();
+    band.initDefaultChannel();
 
-})();
+    band.onMsg=function(data){
+        sonic_panel.innerHTML='Get the string: '+data;
+    };
+    band.onStartReceive=function(){
+        sonic_panel.innerHTML='receiving the sonic data ...... ';
+    };
+
+    navigator.webkitGetUserMedia({
+        audio:{optional:[{echoCancellation:false}]}
+    },function(stream){
+        _input = band.AudioContext.createMediaStreamSource(stream);
+        band.listenSource(_input);
+        band.scanEnvironment();
+    },function(e){});
+}
