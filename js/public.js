d4479f2bd55a0c60cf03283946cdcbb1054bae13
Arthus <yulhuang@cisco.com>
2015-02-12 08:18:42 +0800

    bug2

--- js/public.js
+++ js/public.js
@@ -3,6 +3,6 @@
     var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "//hm.baidu.com/hm.js?dfaf86a3cc463335240ffd085baf4402";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm, s);})();
 })();
 //ga
-(function)(){
+(function(){
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-58407725-1', 'auto');ga('send', 'pageview');
 })();
